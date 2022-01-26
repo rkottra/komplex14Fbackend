d909711bd46cfe1448f5bc36df989b2577d50e10
Kottra Richárd <kottra.richard@jedlik.eu>
2022-01-26 14:02:33 +0100

    kisebb javítások, cors, stb.

--- app/Http/Controllers/Pilota.php
+++ app/Http/Controllers/Pilota.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\DB;
 class Pilota extends Controller
 {
     public function getPilotak() {
-        return DB::select("SELECT * FROM versenyzok");
+        return DB::select("SELECT versenyzok.ID, versenyzok.nev, versenyzok.nemzet, versenyzok.magassag,
+                                versenyzok.szuletes, csapatok.csapatID, 
+                                csapatok.csapatnev, csapatok.nemzet AS 'csapatnemzet'
+                            FROM versenyzok 
+                            INNER JOIN csapatok ON csapatok.csapatId = versenyzok.csapat");
     }
 }
